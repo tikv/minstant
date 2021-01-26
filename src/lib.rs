f5ff59b64a1060ab01734d7e61c69b214dde8382
Zhenchi <zhongzc_arch@outlook.com>
2021-01-27 01:39:44 +0800

    introduce cycle and anchor (#4)
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,63 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 mod coarse_now;
 #[cfg(all(target_os = "linux", any(target_arch = "x86", target_arch = "x86_64")))]
 mod tsc_now;
+
+#[derive(Copy, Clone, Default, Ord, PartialOrd, Eq, PartialEq, Debug)]
+pub struct Cycle(pub u64);
+
+impl Cycle {
+    #[inline]
+    pub fn now() -> Self {
+        Self(now())
+    }
+
+    #[inline]
+    pub fn zero() -> Self {
+        Self(0)
+    }
+
+    #[inline]
+    pub fn is_zero(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn into_unix_time_ns(self, anchor: Anchor) -> u64 {
+        if self > anchor.cycle {
+            let forward_ns = ((self.0 - anchor.cycle.0) as f64 * 1_000_000_000.0
+                / anchor.cycles_per_second as f64) as u64;
+            anchor.unix_time_ns + forward_ns
+        } else {
+            let backward_ns = ((anchor.cycle.0 - self.0) as f64 * 1_000_000_000.0
+                / anchor.cycles_per_second as f64) as u64;
+            anchor.unix_time_ns - backward_ns
+        }
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+pub struct Anchor {
+    pub unix_time_ns: u64,
+    pub cycle: Cycle,
+    pub cycles_per_second: u64,
+}
+
+impl Anchor {
+    #[inline]
+    pub fn new() -> Anchor {
+        let cycle = Cycle(now());
+        let unix_time_ns = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("unexpected time drift")
+            .as_nanos() as u64;
+        Anchor {
+            unix_time_ns,
+            cycle,
+            cycles_per_second: cycles_per_second(),
+        }
+    }
+}
 
 #[inline]
 pub fn tsc_available() -> bool {
